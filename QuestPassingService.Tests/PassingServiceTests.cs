30ee69b65f71353690d66636a2bc54a72d9e2e75
Savelii Korniiko <saveliykorniko@gmail.com>
2025-05-31 19:57:36 +0300

    unit test quest passing microservice

---
+++ QuestPassingService.Tests/PassingServiceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace QuestPassingService.Tests;
+
+public class PassingServiceTests
+{
+    [Fact]
+    public void StartQuest_ShouldInitializeProgress()
+    {
+        var service = new PassingService();
+        var id = Guid.NewGuid();
+
+        var progress = service.StartQuest(id);
+
+        Assert.Equal(id, progress.QuestId);
+        Assert.Equal(0, progress.CompletedTasks);
+        Assert.True(progress.TotalTasks > 0);
+    }
+
+    [Fact]
+    public void AnswerTask_ShouldIncrementProgress()
+    {
+        var service = new PassingService();
+        var id = Guid.NewGuid();
+        service.StartQuest(id);
+
+        var progress = service.AnswerTask(id);
+
+        Assert.NotNull(progress);
+        Assert.Equal(1, progress!.CompletedTasks);
+    }
+}
